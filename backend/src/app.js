5d29331211fafb72b1ed7a071d750f3ba0ae8fa5
Yusufchakkiwala <yusufchakki3@gmail.com>
2026-04-29 12:34:01 +0530

    connect frontend and backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin:"http://your-frontend.vercel.app" || 'http://localhost:5173',
     credentials: true
   })
 );
